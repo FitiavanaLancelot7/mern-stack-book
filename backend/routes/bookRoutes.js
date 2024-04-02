98aec51849426eab291e133d69617309e6630ddc
Fitiavana Lancelot <fitiavanalancelot0@gmail.com>
2024-04-02 15:08:55 +0300

    deployed into VERCEL

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -30,7 +30,7 @@ router.post('/', async (req, res) => {
 // Route for Get All Books from database
 router.get('/', async (req, res)=>{
     try {
-        const books = await Book.find({});
+        // const books = await Book.find({});
         return res.status(201).json({
             count: "Ok",
             data: "😁⚡"
