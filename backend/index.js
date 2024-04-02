791109d83c8e0777ee16c1c4dd700cdb9aff6310
Fitiavana Lancelot <fitiavanalancelot0@gmail.com>
2024-04-02 12:07:37 +0300

    deployed into VERCEL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ App.use(expres.json());
 // App.use(cors());
 // Option 2: custom cors(*)
 App.use(cors({
-    origin: "http://localhost:5000",
+    origin: "https://lancelot-mern.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ['Content-Type'],
 }))
